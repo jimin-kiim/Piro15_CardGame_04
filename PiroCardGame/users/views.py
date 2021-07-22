20577c83f25014ec90b1b05c42b3951a3f795c63
Jeonghoon Park <shb03323@naver.com>
2021-07-22 22:33:53 +0900

    [ADD] users/views.py

--- PiroCardGame/users/views.py
+++ PiroCardGame/users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate
+from .models import User
 
-# Create your views here.
+def attack(request):
+    pass
+
+def counterattack(request):
+    pass
+
+def gameinfo(request):
+    pass
+
+def gamelist(request):
+    pass
+
+def login(request):
+    return render(request, "users/login.html")
+
+def main(request):
+    return render(request, "users/main.html")
+
+def ranking(request):
+    users = User.objects.all()
+    users.sort(key = lambda x: x.score)
+    ctx = {
+        'users':users
+    }
+    return render(request, "users/ranking.html", ctx)
