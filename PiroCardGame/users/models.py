642d3847c0ea24df5572025cd117809ac2802a33
Jimin Kim <0305jimin@gmail.com>
2021-07-23 00:13:51 +0900

    models.py

--- PiroCardGame/users/models.py
+++ PiroCardGame/users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 import random
 
 
-#     PLAYER_CHOICES = (
 class User(models.Model):
     PLAYER_CHOICES = (
         ("player1", "송경"),
@@ -12,65 +11,40 @@ class User(models.Model):
         ("player5", "지민"),
     )
     name = models.CharField(max_length=20, choices=PLAYER_CHOICES)
-    score = models.IntegerField(default=0)
+    choice = models.ForeignKey(to='users.Card')
+    sum = models.IntegerField()
+    result = models.BooleanField()
 
     def __str__(self):
         return self.name
 
-from django.db import models
-import random
+
+class Game(models.Model):
+
+    challenger = models.ForeignKey(to='users.User', on_delete=models.CASCADE)
+    opponent = models.ForeignKey(to='users.User', on_delete=models.CASCADE)
+    # challenger_choice = models.ForeignKey(to='users.Card')
+    # opponent_choice = models.ForeignKey(to='users.Card')
+    status = models.IntegerField()
+    # result = models.BooleanField()
 
 
-class User(models.Model):
-    PLAYER_CHOICES = (
-        ("player1", "송경"),
-        ("player2", "지수"),
-        ("player3", "건모"),
-        ("player4", "정훈"),
-        ("player5", "지민"),
+class Card(models.Model):
+
+    option = []
+    rnum = random.randint(0, 10)
+
+    for i in range(5):
+        while rnum in option:
+            rnum = random.randint(0, 10)
+        option.append(rnum)
+
+    CHOICES = (
+
+        ("num1", ''),
+        ("num2", ''),
+        ("num3", ''),
+        ("num4", ''),
+        ("num5", ''),
+
     )
-    name = models.CharField(max_length=20, choices=PLAYER_CHOICES)
-
-    def __str__(self):
-        return self.name
-
-
-class Game(models.Model):
-    NUM_CHOICES = []
-    rnum = random.randint(0, 10)
-
-    for i in range(5):
-        while rnum in NUM_CHOICES:
-            rnum = random.randint(0, 10)
-        NUM_CHOICES.append(rnum)
-
-    challenger = models.ForeignKey(
-        to='users.User', on_delete=models.CASCADE, related_name='game')
-    opponent = models.ForeignKey(
-        to='users.User', on_delete=models.CASCADE, related_name='game')
-
-    challenger_choice = models.IntegerField(choices=NUM_CHOICES)
-    opponent_choice = models.IntegerField(choices=NUM_CHOICES)
-
-    status = models.IntegerField()
-    result = models.BooleanField()
-
-class Game(models.Model):
-    NUM_CHOICES = []
-    rnum = random.randint(0, 10)
-
-    for i in range(5):
-        while rnum in NUM_CHOICES:
-            rnum = random.randint(0, 10)
-        NUM_CHOICES.append(rnum)
-
-    challenger = models.ForeignKey(
-        to='users.User', on_delete=models.CASCADE, related_name='game')
-    opponent = models.ForeignKey(
-        to='users.User', on_delete=models.CASCADE, related_name='game')
-
-    challenger_choice = models.IntegerField(choices=NUM_CHOICES)
-    opponent_choice = models.IntegerField(choices=NUM_CHOICES)
-
-    status = models.IntegerField()
-    result = models.BooleanField()
