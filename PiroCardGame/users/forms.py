d627be1f730d1d6f65299b510fe61bc13ee11ec0
Jisu Kim <jso_osj@naver.com>
2021-07-22 22:42:24 +0900

    [ADD] users-url

--- PiroCardGame/users/forms.py
+++ PiroCardGame/users/forms.py
@@ -7,3 +7,4 @@ class LoginForm(forms.Form):
     name = models.CharField(max_length=20, verbose_name="ID")
     email = models.EmailField(verbose_name="E-mail")
     password = forms.CharField(widget=forms.PasswordInput)
+    
--- PiroCardGame/users/urls.py
+++ PiroCardGame/users/urls.py
@@ -5,9 +5,10 @@ from . import views
 app_name="users"
 
 urlpatterns = [
-    path('main/', name="main"),
+    path('main/', views.main, name="main"),
     path('login/', views.login, name="login"),
-    path('gamelist/', name="gamelist"),
-    path('attack/', name="attack"), #게임하기 버튼 누르면 가는 곳
-    path('gameinfo/', name="gameinfo") #게임정보 버튼 누르면 가는 곳
+    path('gamelist/', views.gamelist, name="gamelist"),
+    path('attack/', views.attack, name="attack"), #게임하기 버튼 누르면 가는 곳
+    path('gameinfo/', views.gameinfo, name="gameinfo"), #게임정보 버튼 누르면 가는 곳
+    path('ranking/', views.ranking, name="ranking"),
 ]
