4c027f2851bf31ca6ecf1ad62d398149f87c862f
Jeonghoon Park <shb03323@naver.com>
2021-07-22 23:44:53 +0900

    [Edit] users/view.py & base.html

--- PiroCardGame/users/views.py
+++ PiroCardGame/users/views.py
@@ -1,21 +1,47 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 from .models import User
+from . import forms
 
 def attack(request):
-    pass
+    return render(request, "users/attack.html")
 
 def counterattack(request):
-    pass
+    return render(request, "users/counterattack.html")
 
 def gameinfo(request):
-    pass
+    return render(request, "users/gameinfo.html")
 
 def gamelist(request):
-    pass
+    return render(request, "users/gamelist.html")
 
-def login(request):
-    return render(request, "users/login.html")
+def log_in(request):
+    if request.method == "POST":
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            name = form.changed_data.get('name')
+            password = form.cleaned_data.get('password')
+            user = authenticate(name=name, password=password)
+
+            if user is not None:
+                login(request, user)
+                return render(request, "users/main.html")
+            else:
+                return render(request, "users/login.html", {
+                    'error': 'Username or Password is incorrect.',
+                })
+            
+        else:
+            return render(request, "login.html")
+
+    form = forms.LoginForm()
+    return render(request, "users/login.html", {'form':form})
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("users:main"))
+
 
 def main(request):
     return render(request, "users/main.html")
