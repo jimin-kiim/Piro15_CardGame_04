dd6dd5602690b09e1602a9117a0ad018b5a00397
Subeen Park <psb021414@gmail.com>
2021-07-24 20:56:31 +0900

    models code  review

--- PiroCardGame/users/models.py
+++ PiroCardGame/users/models.py
@@ -2,28 +2,24 @@ from django.db import models
 import random
 from django.contrib.auth.models import AbstractUser
 
+
+# FIXME: AbstractUser -> AbstractBaseUser
 class User(AbstractUser):
-    PLAYER_CHOICES = (
-        ("player1", "송경"),
-        ("player2", "지수"),
-        ("player3", "건모"),
-        ("player4", "정훈"),
-        ("player5", "지민"),
-    )
-    name = models.CharField(max_length=20, choices=PLAYER_CHOICES)
-    sum = models.IntegerField(default=0)
+    
+    sum = models.IntegerField(default=0) # FIXME: sum 변수명 바꿔주세요!
 
     def __str__(self):
         return self.username
 
 
+    # 앱 쪼개기
 class Game(models.Model):
     challenger = models.ForeignKey(
-        to='users.User', on_delete=models.CASCADE, related_name='challenger', null=True, default=None)
+        to='users.User', on_delete=models.CASCADE, related_name='challenger', null=True, default=None) # Lazy relation (1) 상호참조 (2) 자기참조 
     opponent = models.ForeignKey(
         to='users.User', on_delete=models.CASCADE, related_name='opponent', null=True)
     status = models.BooleanField(default=True)  # 진행중, 게임 끝
     rule = models.BooleanField(null=True, default=True)    
     result = models.BooleanField(default=True)
-    challengerCard = models.IntegerField(default=1)
-    opponentCard = models.IntegerField(default=0)
+    challenger_card = models.IntegerField(default=1)
+    opponent_card = models.IntegerField(default=0) # camelCase snake_case # default -> 유효한 값 
